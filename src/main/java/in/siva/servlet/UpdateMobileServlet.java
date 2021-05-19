e26569c26391b6ca10ce97178dbdd1b13ffe2357
Siva Murugan B <sivamurugan5101999@gmail.com>
2021-05-19 17:15:53 +0530

    code smell fixed

--- src/main/java/in/siva/servlet/UpdateMobileServlet.java
+++ src/main/java/in/siva/servlet/UpdateMobileServlet.java
@@ -1,7 +1,6 @@
 package in.siva.servlet;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
