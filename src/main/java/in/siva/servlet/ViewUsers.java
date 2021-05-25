5ca1bf51cf124a7fe1aff849b19168ee48209bbf
Siva Murugan B <sivamurugan5101999@gmail.com>
2021-05-25 12:06:49 +0530

    View User feature with JSON Object added

---
+++ src/main/java/in/siva/servlet/ViewUsers.java
@@ -0,0 +1,48 @@
+package in.siva.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import in.siva.exception.DBException;
+import in.siva.model.UserDetail;
+import in.siva.service.UserService;
+
+/**
+ * Servlet implementation class ViewUsers
+ */
+@WebServlet("/ViewUsers")
+public class ViewUsers extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response){
+		Gson users = new Gson();
+		JsonObject object = new JsonObject();
+		try {
+			PrintWriter out = response.getWriter();
+			List<UserDetail> userList = UserService.getAllUsers();
+			String jsonUsers = users.toJson(userList);
+			
+			out.println(jsonUsers);
+			
+		
+		} catch (DBException | IOException e) {
+			e.printStackTrace();
+			object.addProperty("errorMessage", e.getMessage());
+		}
+		
+	}
+
+}
